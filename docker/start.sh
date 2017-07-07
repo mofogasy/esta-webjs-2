d3b431c7a607ffcf61c08b641737e8054c02c3a3
u220374 <reto.lehmann@sbb.ch>
2017-07-07 15:14:14 +0200

    Fixed wrong start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -2,7 +2,7 @@
 
 # replace static values with environment-variables
 if [ -n "$AUTHSERVER_URL" ]; then
-    sed -i "s#http://localhost:9999/#$AUTHSERVER_URL#g" /usr/share/nginx/html/app.*.js
+    sed -i "s#http://localhost:9999/#$AUTHSERVER_URL#g" /usr/share/nginx/html/main.*.bundle.js
 fi
 
 # start webserver
