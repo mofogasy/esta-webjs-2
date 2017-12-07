46891a098d3ed233030dd658205d6c1bfdb5f10b
kreuzerk <kevin.kreuzer90@icloud.com>
2017-12-07 20:21:12 +0100

    Update dependencies

--- src/app/example/about/posts.service.ts
+++ src/app/example/about/posts.service.ts
@@ -20,10 +20,10 @@ export class PostsService {
     }
 
     getAllPosts(): Observable<Array<Post>> {
-        return this.http.get(this.ENDPOINT_URL);
+        return this.http.get(this.ENDPOINT_URL) as Observable<Array<Post>>;
     }
 
     getPostById(id): Observable<Post> {
-        return this.http.get(`${this.ENDPOINT_URL}/${id}`);
+        return this.http.get(`${this.ENDPOINT_URL}/${id}`) as Observable<Post>;
     }
 }
