b232e484ec6981d1a16d445272c07d0a4e42c2b4
u220374 <reto.lehmann@sbb.ch>
2018-05-30 16:06:36 +0200

    Testing e2e on jenkins.

--- e2e/protractor.webgrid.conf.js
+++ e2e/protractor.webgrid.conf.js
@@ -7,41 +7,40 @@ let hostname = process.env.host || require('my-local-ip')();
 let externalport = process.env.externalport;
 
 exports.config = {
-    allScriptsTimeout: 11000,
-    specs: [
-        './e2e/**/*.e2e-spec.ts'
-    ],
-    multiCapabilities: [
-        {
-            browserName: 'chrome'
-        },
-        {
-            browserName: 'firefox'
-        }
-    ],
-    baseUrl: 'http://' + hostname + ':' + externalport,
-    seleniumAddress: 'http://webtestgrid.sbb.ch:4444/wd/hub',
-    framework: 'jasmine',
-    jasmineNodeOpts: {
-        showColors: true,
-        defaultTimeoutInterval: 30000,
-        print: function () {
-        }
+  allScriptsTimeout: 11000,
+  specs: [
+    './src/**/*.e2e-spec.ts'
+  ],
+  capabilities: [
+    {
+      browserName: 'chrome'
     },
-    beforeLaunch: function () {
-    },
-    onPrepare(){
-        require('ts-node').register({
-            project: 'e2e/tsconfig.e2e.json'
-        });
-        jasmine.getEnv().addReporter(new SpecReporter({spec: {displayStacktrace: true}}));
-        jasmine.getEnv().addReporter(new (require('jasmine-reporters').JUnitXmlReporter)({
-            filePrefix: 'e2e-tests',
-            savePath: 'reports',
-            package: 'e2e',
-            modifySuiteName: function (generatedSuiteName) {
-                return 'e2e.' + generatedSuiteName.replace(/\./g, '#');
-            }
-        }));
+    {
+      browserName: 'firefox'
     }
+  ],
+  directConnect: true,
+  baseUrl: 'http://' + hostname + ':' + externalport,
+  seleniumAddress: 'http://webtestgrid.sbb.ch:4444/wd/hub',
+  framework: 'jasmine',
+  jasmineNodeOpts: {
+    showColors: true,
+    defaultTimeoutInterval: 30000,
+    print: function () {
+    }
+  },
+  onPrepare() {
+    require('ts-node').register({
+      project: require('path').join(__dirname, './tsconfig.e2e.json')
+    });
+    jasmine.getEnv().addReporter(new SpecReporter({spec: {displayStacktrace: true}}));
+    jasmine.getEnv().addReporter(new (require('jasmine-reporters').JUnitXmlReporter)({
+      filePrefix: 'e2e-tests',
+      savePath: 'reports',
+      package: 'e2e',
+      modifySuiteName: function (generatedSuiteName) {
+        return 'e2e.' + generatedSuiteName.replace(/\./g, '#');
+      }
+    }));
+  }
 };
